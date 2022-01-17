95b14b42a4a5d880e8cce871ed60764c95e11faf
Ranu2211 <88536226+Ranu2211@users.noreply.github.com>
2022-01-18 03:26:46 +0530

    Time: 616 ms (12.47%), Space: 145.6 MB (27.13%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -6,10 +6,11 @@ public:
        priority_queue<pair<int,int>>max;
          vector<int>v;
        for(int i=0;i<nums.size();i++){
+           max.push(make_pair(nums[i],i));
             while(!max.empty() && max.top().second<=(i-k))
                 max.pop();
-                max.push(make_pair(nums[i],i));
-                if(i>=k-1)
+                //max.push(make_pair(nums[i],i));
+                if(i+1>=k)
                     v.push_back(max.top().first);
             
         }

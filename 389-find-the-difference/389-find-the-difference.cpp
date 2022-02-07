29978168896037cd021c0b40e311f247e4488c97
Ranu2211 <88536226+Ranu2211@users.noreply.github.com>
2022-02-07 21:21:14 +0530

    Time: 3 ms (78.44%), Space: 6.8 MB (9.15%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char v = 0;
-        for (char c: s) v ^= c;
-        for (char c: t) v ^= c;
-        return v;
-    
+       unordered_map<char,int>mp;
+        char a;
+        for(int i=0;i<s.length();i++){
+            mp[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++)
+            mp[t[i]]++;
+        for(auto i:mp){
+            if(i.second%2==1){
+                a = i.first;
+            }
+        }
+    return a;
     }
 };

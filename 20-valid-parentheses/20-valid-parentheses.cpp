4e63da1789376a985d8a6e0d260c4dadeca21c07
Ranu2211 <88536226+Ranu2211@users.noreply.github.com>
2022-04-19 19:36:08 +0530

    Time: 2 ms (42.40%), Space: 6.4 MB (51.47%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -4,30 +4,31 @@ public:
         stack<char>st;
         for(int i=0;i<s.length();i++){
             char ch = s[i];
-            if(ch== '(' || ch=='{' || ch=='['){
+            if(ch=='(' || ch=='{' || ch=='['){
                 st.push(ch);
             }
             else{
                 if(!st.empty()){
                     char top = st.top();
-                    if((ch== ')' && top == '(') ||
-                       (ch== '}' && top == '{') ||
-                        (ch== ']' && top == '[')){
-                           st.pop();
+                    if((ch==')' && top == '(')||
+                      (ch=='}' && top == '{')||
+                      (ch==']' && top == '[')){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
                 }
-                        else{
-                            return false;
-                        }
-                     }
-                  else{
-                   return false;
+                else{
+                    return false;
+                }
+            } 
             }
-             }
-        }
-        if(st.empty())
-            return true;
-        else
-            return false;
-    }                   
-    
+            if(st.empty())
+                return true;
+                    return false;
+                
+            
+        
+   }
 };

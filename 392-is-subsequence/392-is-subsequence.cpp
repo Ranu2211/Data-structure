bb9f47387a893f25a86e3d077c46ae652e9abbb0
Ranu2211 <88536226+Ranu2211@users.noreply.github.com>
2022-05-10 19:15:56 +0530

    Time: 3 ms (45.43%), Space: 6.8 MB (18.65%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-     bool isSubsequence(string s, string t) {
-        int m = s.size();
-        int n = t.size();
-        int i = 0, j = 0;
-        while(i < m && j < n) {
-            if(s[i] == t[j]) {
-                i++;
-            }
-            j++;
-        }
-        return i == m ? 1 : 0;
+     int lcs(int x, int y, string s1, string s2)
+    {
+        int t[x+1][y+1];
+        for(int i=0;i<x+1;i++)
+        t[i][0] = 0;
+       for(int j=0;j<y+1;j++)
+       t[0][j] =0;
+       for(int i=1;i<x+1;i++)
+       for(int j=1;j<y+1;j++)
+       if(s1[i-1]==s2[j-1])
+       t[i][j] = 1+ t[i-1][j-1];
+       else
+       t[i][j] = max(t[i-1][j],t[i][j-1]);
+       return t[x][y];
+    }
+    bool isSubsequence(string s, string t) {
+        int x = s.length();
+        int y = t.length();
+        if(lcs(x,y,s,t)==x)
+            return true;
+        return false;
     }
 };

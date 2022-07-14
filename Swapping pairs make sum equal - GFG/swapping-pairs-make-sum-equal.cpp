2e0d53d248091d4c92a3d8da91b5d2dca0cd86b4
Ranu2211 <88536226+Ranu2211@users.noreply.github.com>
2022-07-14 23:19:51 +0530

    Added solution - LeetHub

--- Swapping pairs make sum equal - GFG/swapping-pairs-make-sum-equal.cpp
+++ Swapping pairs make sum equal - GFG/swapping-pairs-make-sum-equal.cpp
@@ -9,25 +9,28 @@ class Solution{
 	int findSwapValues(int A[], int n, int B[], int m)
 	{
 	    sort(A,A+n);
-	    sort(B,B+m);
-        int sumA=0,sumB=0 ,i=0,j=0;
-        for(int i=0;i<n;i++){
-            sumA+=A[i];
-        }
+	     sort(B,B+m);
+	    
+	    int sumA=0 ,sumB=0,diffA,diffB;
+       for(int i=0;i<n;i++){
+           sumA +=A[i];
+       }
         for(int i=0;i<m;i++){
-            sumB+=B[i];
-        }
-        while(i<n && j<m){
-            long p = sumA-A[i]+B[j];
-            long q = sumB-B[j]+A[i];
-            if(p==q)
-            return 1;
-           else if(p>q)
-            i++;
-            else
-            j++;
-        }
-        return -1;
+           sumB +=B[i];
+       }
+       int i=0 ,j=0;
+       while(i<n && j<m){
+           long diffA = sumA-A[i]+B[j];
+            long diffB = sumB-B[j]+A[i];
+       if(diffA==diffB)
+       return 1;
+       else if(diffA>diffB)
+       i++;
+       else
+       j++;
+       }
+       return -1;
+       
 	}
  
 
